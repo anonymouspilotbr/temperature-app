f26f4194a1318250a93802452833fc253e5de767
anonymouspilotbr <ybrasileiro6@gmail.com>
2026-06-23 10:46:21 -0300

    [v004] Sort pipe

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -6,7 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TemperaturePipe implements PipeTransform {
 
-  transform(value: string | number, inputType: 'cel' | 'fah', outputType?: 'cel' | 'fah') {
+  transform(value: string | number | null, inputType: 'cel' | 'fah', outputType?: 'cel' | 'fah') {
+    if(!value){
+      return value;
+    }
+    
     let val: number;
 
     if(typeof value === 'string'){
@@ -33,7 +37,7 @@ export class TemperaturePipe implements PipeTransform {
       symbol = outputType === 'cel' ? '°C' : '°F';
     }
 
-    return `${outputTemp} ${symbol}`;
+    return `${outputTemp.toFixed(2)} ${symbol}`;
   }
 
 }
